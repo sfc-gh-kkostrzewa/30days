add209e394e632346153f412737ad908c2eb53e4
Karol Kostrzewa <karol.kostrzewa@snowflake.com>
2024-11-18 12:51:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-st.write("test 12345")
+st.write("test 123456")
 def update_params():
     st.experimental_set_query_params(challenge=st.session_state.day)
 
